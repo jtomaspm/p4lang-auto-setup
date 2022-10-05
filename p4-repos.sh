834f3e429a9dcb8972569a8ab445a077a34dd0e8
João Machado <76636822+jtomaspm@users.noreply.github.com>
2022-10-05 12:40:02 +0100

    Update p4-repos.sh

--- p4-repos.sh
+++ p4-repos.sh
@@ -9,13 +9,13 @@ export DEBIAN_FRONTEND=noninteractive
 # Add repository with P4 packages
 # https://build.opensuse.org/project/show/home:p4lang
 
-echo "deb http://download.opensuse.org/repositories/home:/p4lang/Debian_11/ /" | sudo tee /etc/apt/sources.list.d/home:p4lang.list
-wget -qO - "http://download.opensuse.org/repositories/home:/p4lang/Debian_11/Release.key" | apt-key add -
+echo "deb http://download.opensuse.org/repositories/home:/p4lang/xUbuntu_20.04/ /" | sudo tee /etc/apt/sources.list.d/home:p4lang.list
+wget -qO - "http://download.opensuse.org/repositories/home:/p4lang/xUbuntu_20.04/Release.key" | apt-key add -
 
 apt-get update -qq
 
 
-apt-get  -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+apt-get -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 apt-get install  -y --no-install-recommends --fix-missing\
   curl \
   git \
